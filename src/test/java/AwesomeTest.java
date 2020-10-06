d10a2ffd470b3c866b3685c4a14778da6394a32f
Sinan Altuntas <sinanaltuntas.ist@gmail.com>
2020-10-06 22:59:23 +0300

    Added more awesome stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -5,4 +5,10 @@ public class AwesomeTest {
     //grey in .idea folder means it's ignored
     //when the color is normal, it means it's committed no changes
 
+
+    //I just pulled the new changes from github that i did not have locally
+    //changes
+
+    //new changes
+
 }
